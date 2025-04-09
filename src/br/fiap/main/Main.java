95611ffeba17cff1e565c90cacf2d657bf031988
IsaqueZaack <isaquesantanapaixao444@gmail.com>
2025-04-09 09:44:00 -0300

    Projeto finalizado

--- src/br/fiap/main/Main.java
+++ src/br/fiap/main/Main.java
@@ -1,7 +1,12 @@
 package br.fiap.main;
 
+import br.fiap.Util.Util;
+
 public class Main {
     public static void main(String[] args) {
 
+        Util util = new Util();
+
+        util.menu();
     }
 }
